22f2889a3defaad9e838d2ebf5212e7b662491f0
Huynh <huynhkin269@gmail.com>
2022-12-13 15:24:27 +0700

    view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { PollComponent } from './components/poll/poll.component';
+import { VoteComponent } from './components/vote/vote.component';
+import { ItemComponent } from './components/item/item.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'item', component:ItemComponent},
+  {path: 'vote', component:VoteComponent},
+  {path: 'poll', component:PollComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { ItemComponent } from './components/item/item.component';
+import { VoteComponent } from './components/vote/vote.component';
+import { PollComponent } from './components/poll/poll.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemComponent,
+    VoteComponent,
+    PollComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
